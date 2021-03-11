f477030710e1aca6c2051970dae17c8332ecc2f8
TassioSales <74218122+TassioSales@users.noreply.github.com>
2021-03-11 01:55:56 -0300

    atualizando jogo

--- parte 01/forca.py
+++
@@ -1,9 +0,0 @@
-def jogar_forca():
-    print("*********************************")
-    print("Bem vindo ao jogo de Adivinhação!")
-    print("*********************************")
-
-    print("fim de jogo")
-    
-if(__name__ == "__main__"):
-    jogar_forca()
