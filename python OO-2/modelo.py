3edca6647849f152be7d9b7bb7f230a6946b26eb
TassioSales <74218122+TassioSales@users.noreply.github.com>
2021-03-16 11:31:32 -0300

    atualiza

---
+++ python OO-2/modelo.py
@@ -0,0 +1,20 @@
+class Filme:
+    def __init__(self, nome, ano, duracao):
+        self.nome = nome
+        self.ano = ano
+        self.duracao = duracao
+
+
+class Serie:
+    def __init__(self, nome, ano, temporadas):
+        self.nome = nome
+        self.ano = ano
+        self.temporadas = temporadas
+
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+atlanta = Serie('Atlanta', 2018, 2)
+
+
+print(f'Nome: {atlanta.nome} - Ano {atlanta.ano} - Temporada {atlanta.temporadas}')
+print(f'Nome: {vingadores.nome} - Ano {vingadores.ano} - Duração: {vingadores.duracao}')
