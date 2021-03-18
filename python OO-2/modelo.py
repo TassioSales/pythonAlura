265086d94f7f7a933eecc876274bbf1d384a0f1a
TassioSales <74218122+TassioSales@users.noreply.github.com>
2021-03-17 22:04:54 -0300

    atualizando

--- python OO-2/modelo.py
+++ python OO-2/modelo.py
@@ -1,20 +1,60 @@
-class Filme:
-    def __init__(self, nome, ano, duracao):
-        self.nome = nome
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
         self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_likes(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self._likes}')
+
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
         self.duracao = duracao
 
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self.duracao} - {self._likes}')
 
-class Serie:
+
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
+
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self.temporadas} - {self._likes}')
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+vingadores.dar_likes()
+vingadores.dar_likes()
+
 atlanta = Serie('Atlanta', 2018, 2)
+atlanta.dar_likes()
+atlanta.dar_likes()
+atlanta.dar_likes()
+
+filmes_e_series = list([vingadores, atlanta])
+
+for itens in filmes_e_series:
+    itens.imprime()
 
 
-print(f'Nome: {atlanta.nome} - Ano {atlanta.ano} - Temporada {atlanta.temporadas}')
-print(f'Nome: {vingadores.nome} - Ano {vingadores.ano} - Duração: {vingadores.duracao}')
+
+
