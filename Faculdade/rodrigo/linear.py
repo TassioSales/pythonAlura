7b95e80f584e3dc805c8a8c18ff70f3c32df1a4a
TassioSales <74218122+TassioSales@users.noreply.github.com>
2021-03-20 12:52:44 -0300

    troca de pasta

--- Faculdade/rodrigo/linear.py
+++
@@ -1,9 +0,0 @@
-def linearsearch(arr, x):
-   for i in range(len(arr)):
-      if arr[i] == x:
-         return i
-   return -1
-nome = ['f','l','a','m','e','n','g','o']
-letra = 'g'
-
-print(f'O Elemento encontrado no indice {linearsearch(nome,letra)}')
